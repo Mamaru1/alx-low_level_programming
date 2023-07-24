0becc8e03faf74e49b95880a58897724fcbf702e
Mamaru1 <mamaru0920@gmail.com>
2023-07-24 12:58:08 -0700

    a function that takes a pointer to an int as parameter and updates the value it points to to 98.

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -18,13 +18,19 @@ void rev_string(char *s)
 	}
 	len = a + 1;
 	begin = s;
+
 	for (a = 0; a < len / 2; a++)
 	{
 		char x;
+
 		x = *end;
+
 		*end = *begin;
+
 		*begin = x;
+
 		begin++;
+
 		end--;
 	}
 	end[len + 1] = '\0';
